d6deef6d8526aceb68577d614ab1adc310aab9d0
Mehrad <mehradhetfield@gmail.com>
2023-11-13 06:46:47 +0330

    feat: http req hook and auth context added

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [accessToken, setAccessToken] = useState(null);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        accessToken,
+        setAccessToken,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
