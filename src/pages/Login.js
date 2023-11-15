1effd3196da242d5e7f762e7a6f2be3c6dae551b
unknown <mehradhetfield@gmail.com>
2023-11-15 05:06:00 -0800

    refactor: old login removed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,7 @@
-import LoginForm from "../components/Login/LoginForm";
-import backgroundImage from '../assets/images/image2.jpg'
+
 const Login = () => {
     return ( 
         <div className="row login-bg"  dir="rtl">
-            <LoginForm />
         </div>
      );
 }
